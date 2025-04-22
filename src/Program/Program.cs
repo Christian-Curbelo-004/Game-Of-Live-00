a68a026b6ed1557f88ac04646aa1dc7607c1e0c6
Joacoaires <jose.aires@correo.ucu.edu.uy>
2025-04-22 18:52:56 -0300

    mergear board

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Library; 
 
 namespace Ucu.Poo.GameOfLife
 {
@@ -6,7 +8,23 @@ namespace Ucu.Poo.GameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Carga del tablero desde archivo - SRP: se delega a una clase experta en esto.
+            string filePath = "board.txt";
+            bool[,] initialBoard = BoardImporter.ImportBoard(filePath);
+
+            // Se crea el tablero (Board) con los datos iniciales
+            Board board = new Board(initialBoard);
+
+            // Se instancia la lógica del juego con ese tablero
+            GameCore game = new GameCore(board);
+
+            // Bucle infinito para mostrar el juego
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(BoardPrinter.Print(board));
+                game.NextGeneration();
+                Thread.Sleep(300);
+            }
         }
     }
-}
